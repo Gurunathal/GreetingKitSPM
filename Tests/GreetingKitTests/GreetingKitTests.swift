7a4e77b62ecbc73343e29ce1d52c5dc7dccc261e
Gurunath <sumit.allapure@gmail.com>
2026-03-15 23:31:51 +0530

    failing test addded

--- Tests/GreetingKitTests/GreetingKitTests.swift
+++ Tests/GreetingKitTests/GreetingKitTests.swift
@@ -2,5 +2,10 @@ import Testing
 @testable import GreetingKit
 
 @Test func example() async throws {
+    
+    let grit = GreetingKit()
+    let result = grit.greet(name: "World")
+    #expect(result == "Hello, World")
+    
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
