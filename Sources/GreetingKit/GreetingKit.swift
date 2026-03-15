55f05e8962eb163e539246affca9ba0ed875479c
Gurunath <sumit.allapure@gmail.com>
2026-03-15 21:12:07 +0530

    Test SwiftLint CI

--- Sources/GreetingKit/GreetingKit.swift
+++ Sources/GreetingKit/GreetingKit.swift
@@ -3,12 +3,6 @@
 
 public struct GreetingKit {
     public init() {}
-    
-    public func greet(name: String) -> String {
-        return "Hello, \(name)! this is a greeting from GreetingKit."
-    }
-    
-    public func bye(name: String) -> String {
-        "Bye, \(name)! this is a goodbye from GreetingKit."
-    }
+
+    public func greet(name:String)->String{ return "Hello \(name)" }
 }
